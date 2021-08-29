92ca6ce0d9f35763b13df6a0c6d46815d5f1feb2
LiangbinChen <chenliangbin123456@126.com>
2021-08-29 20:48:56 +0800

    Adding login and newsSandbox routers

--- newssystem/src/router/indexRouter.js
+++ newssystem/src/router/indexRouter.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom'
+import Login from '../views/login/Login'
+import NewsSandBox from '../views/sanbox/NewsSandBox';
 
-export default function indexRouter() {
+export default function IndexRouter() {
     return (
-        <div>
-            
-        </div>
+        <HashRouter>
+            <Switch>
+                <Route path="/login" component={Login}></Route>
+            {/* <Route path="/" component={NewsSandBox}></Route> */}
+            <Route path="/" render={ () =>
+                localStorage.getItem("token")?
+                <NewsSandBox></NewsSandBox>:
+                <Redirect to="/login"/>
+            }
+            />
+            </Switch>          
+        </HashRouter>
     )
 }
